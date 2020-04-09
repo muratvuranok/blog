c4ac382caf7e4ec9aae83bb3bbc8a8ce2b489e74
muratvuranok <muratvuranok@hotmail.com>
2020-04-09 18:26:28 +0300

    ConnectionString Edit

--- blog.root/CompositionRoot.cs
+++ blog.root/CompositionRoot.cs
@@ -16,7 +16,7 @@ namespace blog.root
             services.AddScoped(typeof(IPostImageRepository), typeof(PostImageService));
             services.AddScoped(typeof(ICategoryRepository), typeof(CategoryService));
             services.AddDbContext<blogcontext>(options =>
-            options.UseSqlServer("server=94.73.170.5;uid=u8924044_blog;database=u8924044_blog; pwd=EGyz66W3RBcx36U", x => x.MigrationsAssembly("blog.data")));
+            options.UseSqlServer("server=;uid=u8924044_blog;database=u8924044_blog; pwd=EGyz66W3RBcx36U", x => x.MigrationsAssembly("blog.data")));
         }
     }
 }
